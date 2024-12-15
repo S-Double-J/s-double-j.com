a51a39d1992711a972904192d3b70d987abd85de
OttahDesgin <samjukes.dev@gmail.com>
2024-12-15 00:28:08 +0000

    fixed TypeScript complaint about unused variable

--- src/tools/CharRandomiser.tsx
+++ src/tools/CharRandomiser.tsx
@@ -8,17 +8,18 @@ const charRandomizer = (event: React.MouseEvent<HTMLElement>) => {
   if (target.tagName !== 'P') {
     target = target.querySelector('p') as HTMLElement;
   }
-
   clearInterval(interval);
 
   interval = window.setInterval(() => {
     if (target && target.dataset.value) {
       target.innerText = target.innerText
         .split("")
-        .map((letter, index) => {
+        .map((l, index) => {
+          console.log(l)
           if (index < iteration) {
             return target.dataset.value![index];
           }
+
 
           return letters[Math.floor(Math.random() * letters.length)];
         })
