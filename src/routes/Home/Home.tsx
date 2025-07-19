6b63b65fa809e44e96fd670c8bf73d14ec2a23ad
S-Double-J <samjukes.dev@gmail.com>
2025-07-19 09:14:17 +0100

    added servhpme back onto mobile

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -5,7 +5,7 @@ import ServHome from "./ServicesHome";
 import Footer from "../../componenets/Footer";
 import { useRef } from "react";
 import { motion } from "motion/react";
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 // import ServHomeMobile from "./ServicesHomeMobile";
 
 const Frame = styled(motion.div)`
